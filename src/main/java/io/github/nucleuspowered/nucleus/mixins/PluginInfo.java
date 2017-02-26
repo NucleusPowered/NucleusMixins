e5851806860af73890c123560f87677f6532fa77
Daniel Naylor <git@drnaylor.co.uk>
2017-02-26 12:42:28 +0000

    Forgot to change this...

--- src/main/java/io/github/nucleuspowered/nucleus/mixins/PluginInfo.java
+++ src/main/java/io/github/nucleuspowered/nucleus/mixins/PluginInfo.java
@@ -11,7 +11,7 @@ public final class PluginInfo {
     public static final String ID = "nucleus-mixin";
 
     public static final String NAME = "Nucleus Mixins";
-    public static final String VERSION = "0.15.0-5.0";
+    public static final String VERSION = "0.25.1-5.1";
 
     // Preparing for 4.0.0 SpongeAPI
     public static final String DESCRIPTION = "Nucleus Mixins";
